0ef88da172e48b54673e5cc8749753a28f634516
farsor <62061782+2human@users.noreply.github.com>
2021-10-06 16:46:38 -0400

    test1

--- src/main/java/com/BootjpaApplication.java
+++ src/main/java/com/BootjpaApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BootjpaApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BootjpaApplication.class, args);
+		SpringApplication.run(BootjpaApplication.class, args); 
 	}
 	
 }
